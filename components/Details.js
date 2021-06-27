1459355d7d6cbe339fa41daf197ce6d8a534af9a
ianlee92 <ethian9227@gmail.com>
2021-06-28 00:29:21 +0900

    infomation

--- components/Details.js
+++ components/Details.js
@@ -44,6 +44,29 @@ export default Details = ({route, navigation}) => {
       <View style={styles.priceWrapper}>
         <Text style={styles.priceText}>${item.price}</Text>
       </View>
+
+      {/* Pizza info */}
+      <View style={styles.infoWrapper}>
+        <View style={styles.infoLeftWrapper}>
+          <View style={styles.infoItemWrapper}>
+            <Text style={styles.infoItemTitle}>Size</Text>
+            <Text style={styles.infoItemText}>
+              {item.sizeName} {item.sizeNumber}"
+            </Text>
+          </View>
+          <View style={styles.infoItemWrapper}>
+            <Text style={styles.infoItemTitle}>Crust</Text>
+            <Text style={styles.infoItemText}>{item.crust}</Text>
+          </View>
+          <View style={styles.infoItemWrapper}>
+            <Text style={styles.infoItemTitle}>Delivery in</Text>
+            <Text style={styles.infoItemText}>{item.deliveryTime}</Text>
+          </View>
+        </View>
+        <View>
+          <Image source={item.image} style={styles.itemImage} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -90,5 +113,33 @@ const styles = new StyleSheet.create({
     color: colors.price,
     fontFamily: 'Montserrat-Bold',
     fontSize: 32,
+  },
+  infoWrapper: {
+    marginTop: 60,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  infoLeftWrapper: {
+    paddingLeft: 20,
+  },
+  infoItemWrapper: {
+    marginBottom: 20,
+  },
+  infoItemTitle: {
+    fontFamily: 'Montserrat-Medium',
+    fontSize: 14,
+    color: colors.textLight,
+  },
+  infoItemText: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 18,
+    color: colors.textDark,
+  },
+  itemImage: {
+    resizeMode: 'contain',
+    marginLeft: 50,
+    width: 300,
+    height: 180,
   },
 });
