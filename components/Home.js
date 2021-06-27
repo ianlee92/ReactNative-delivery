0b21021935eba6975701422060341c191a5b9998
ianlee92 <ethian9227@gmail.com>
2021-06-27 22:21:05 +0900

    popular card section

--- components/Home.js
+++ components/Home.js
@@ -14,6 +14,7 @@ import popularData from '../assets/data/popularData';
 import colors from '../assets/colors/colors';
 
 Feather.loadFont();
+MaterialCommunityIcons.loadFont();
 
 export default Home = () => {
   const renderCategoryItem = ({item}) => {
@@ -84,6 +85,55 @@ export default Home = () => {
             horizontal={true}
           />
         </View>
+      </View>
+
+      {/* Popular */}
+      <View style={styles.popularWrapper}>
+        <Text style={styles.popularTitle}>Popular</Text>
+        {popularData.map(item => (
+          <View
+            style={[
+              styles.popularCardWrapper,
+              {
+                marginTop: item.id == 1 ? 10 : 20,
+              },
+            ]}>
+            <View>
+              <View>
+                <View style={styles.popularTopWrapper}>
+                  <MaterialCommunityIcons
+                    name="crown"
+                    size={12}
+                    color={colors.primary}
+                  />
+                  <Text style={styles.popularTopText}>top of the week</Text>
+                </View>
+                <View style={styles.popularTitlesWrapper}>
+                  <Text style={styles.popularTitlesTitle}>{item.title}</Text>
+                  <Text style={styles.popularTitlesWeight}>
+                    Weight {item.weight}
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.popularCardBottom}>
+                <View style={styles.addPizzaButton}>
+                  <Feather name="plus" size={10} color={colors.textDark} />
+                </View>
+                <View style={styles.ratingWrapper}>
+                  <MaterialCommunityIcons
+                    name="star"
+                    size={10}
+                    color={colors.textDark}
+                  />
+                  <Text style={styles.rating}>{item.rating}</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.popularCardRight}>
+              <Image source={item.image} style={styles.popularCardImage} />
+            </View>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -180,5 +230,75 @@ const styles = StyleSheet.create({
   },
   categorySelectIcon: {
     alignSelf: 'center',
+  },
+  popularWrapper: {
+    paddingHorizontal: 20,
+  },
+  popularTitle: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 16,
+  },
+  popularCardWrapper: {
+    backgroundColor: colors.white,
+    borderRadius: 25,
+    paddingTop: 20,
+    paddingLeft: 20,
+    flexDirection: 'row',
+    overflow: 'hidden',
+  },
+  popularTopWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  popularTopText: {
+    marginLeft: 10,
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 14,
+  },
+  popularTitlesWrapper: {
+    marginTop: 20,
+  },
+  popularTitlesTitle: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 14,
+    color: colors.textDark,
+  },
+  popularTitlesWeight: {
+    fontFamily: 'Montserrat-Medium',
+    fontSize: 12,
+    color: colors.textLight,
+    marginTop: 5,
+  },
+  popularCardBottom: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+    marginLeft: -20,
+  },
+  addPizzaButton: {
+    backgroundColor: colors.primary,
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    borderTopRightRadius: 25,
+    borderBottomLeftRadius: 25,
+  },
+  ratingWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 20,
+  },
+  rating: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 12,
+    color: colors.textDark,
+    marginLeft: 5,
+  },
+  popularCardRight: {
+    marginLeft: 40,
+  },
+  popularCardImage: {
+    width: 210,
+    height: 125,
+    resizeMode: 'contain',
   },
 });
