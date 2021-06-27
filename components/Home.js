d4c519cabd35998e7afd58cc92fc4601bd598e6a
ianlee92 <ethian9227@gmail.com>
2021-06-27 21:14:45 +0900

    search'

--- components/Home.js
+++ components/Home.js
@@ -28,6 +28,13 @@ export default Home = () => {
         <Text style={styles.titleSubtitle}>Food</Text>
         <Text style={styles.titlesTitle}>Delivery</Text>
       </View>
+      {/* Search */}
+      <View style={styles.searchWrapper}>
+        <Feather name="search" size={16} color={colors.textDark} />
+        <View style={styles.search}>
+          <Text style={styles.searchText}>Search</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -62,5 +69,23 @@ const styles = StyleSheet.create({
     fontSize: 32,
     color: colors.textDark,
     marginTop: 5,
+  },
+  searchWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    marginTop: 30,
+  },
+  search: {
+    flex: 1,
+    marginLeft: 10,
+    borderBottomColor: colors.textLight,
+    borderBottomWidth: 2,
+  },
+  searchText: {
+    fontFamily: 'Montserrat-Semibold',
+    fontSize: 14,
+    marginBottom: 5,
+    color: colors.textLight,
   },
 });
